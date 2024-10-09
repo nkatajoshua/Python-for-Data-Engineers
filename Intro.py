267e18d8f2a7faa194e2e1a037fdf40d772404ba
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-10-09 19:21:33 +0300

    Create Intro.py

---
+++ Intro.py
@@ -0,0 +1,24 @@
+#Intro
+
+print(type(revenue_on_target))
+
+# Create a string variable called name
+name = "John Smith"
+
+# Create an integer variable called age
+age = 47
+
+# Create a string variable called address
+address = "The Shard, London, SE1 9SG"
+
+# Print their information
+print(name,age,address)
+
+# Create a boolean variable called is_adult
+is_adult = True
+
+# Update John's address
+address = "Buckingham Palace, London, SW1A 1AA"
+
+# Print their information
+print(is_adult,address)
