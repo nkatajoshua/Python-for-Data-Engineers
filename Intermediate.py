c56644a223875013584c1dea0545ece93c3ff5bb
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-10-10 19:30:46 +0300

    Update Intermediate.py

--- Intermediate.py
+++ Intermediate.py
@@ -98,3 +98,19 @@ def clean_string(text):
 
 converted_text = clean_string("I LoVe dATaCamP!")
 print(converted_text)
+
+password = "not_very_secure_2023"
+
+# Define the password_checker function
+def password_checker(submission):
+  
+  # Check that the password variable and the submission match
+  if password == submission:
+    print("Successful login!")
+  
+  # Otherwise, print "Incorrect password"
+  else:
+    print("Incorrect password")
+
+# Call the function    
+password_checker("NOT_VERY_SECURE_2023")
