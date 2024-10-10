de125cbe5a5a2d1deec440bbdd6424c923205399
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-10-10 18:14:22 +0300

    Update Intermediate.py

--- Intermediate.py
+++ Intermediate.py
@@ -23,3 +23,37 @@ print(title_length)
 
 # Print the total number of course completions
 print(sum(course_completions))
+
+# Print the largest number of completions
+print(max(course_completions))
+
+# Print the average number of completions
+print(sum(course_completions)/ len(course_completions))
+
+# Print the average number of completions, rounded to one decimal places
+print(round(sum(course_completions) / len(course_completions), 1))
+
+
+
+                        #MODULES#
+
+# Import the string module
+import string
+
+# Print all ASCII lowercase characters
+print(string.ascii_lowercase)
+
+# Print all punctuation
+print(string.punctuation)
+
+# Import date from the datetime module
+from datetime import date
+
+# Create a variable called deadline
+deadline = date(2024, 1, 19)
+
+# Check the data type
+print(type(deadline))
+
+# Print the deadline
+print(deadline)
