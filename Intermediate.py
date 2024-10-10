ca32961f559510bb85ff3b1a87789a637a5947f4
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-10-10 19:46:21 +0300

    Update Intermediate.py

--- Intermediate.py
+++ Intermediate.py
@@ -134,3 +134,20 @@ def clean_text(text, remove=None):
   else:
     clean_text = text.lower()
     return clean_text
+  
+# Create the convert_data_structure function
+def convert_data_structure(data, data_type="list"):
+  
+  # If data_type is "tuple"
+  if data_type == "tuple":
+    data = tuple(data)
+  
+  # Else if data_type is set, convert to a set
+  elif data_type == "set":
+    data = set(data)
+  else:
+    data = list(data)
+  return data
+
+# Call the function to convert to a set
+convert_data_structure({"a", 1, "b", 2, "c", 3}, data_type="set")
