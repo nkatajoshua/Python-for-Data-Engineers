f50a7942759aea05008ec796d3d95cee1fd8f7a5
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-10-11 23:20:48 +0300

    Update Importing data in python.py

--- Importing data in python.py
+++ Importing data in python.py
@@ -2,3 +2,9 @@
 with open('moby_dick.txt', 'r') as file:
   	# Print it
     print(file.read())
+
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
