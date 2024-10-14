6a5fce9c7146db1b9c29fb2f7c30b7c4d8be7be0
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-10-15 00:10:17 +0300

    Update matlib.py

--- matlib.py
+++ matlib.py
@@ -23,3 +23,21 @@ plt.plot(gdp_cap,life_exp)
 
 # Display the plot
 plt.show()
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
+
+# Import package
+import matplotlib.pyplot as plt
+
+# Build Scatter plot
+plt.scatter(life_exp,pop)
+
+# Show plot
+plt.show()
