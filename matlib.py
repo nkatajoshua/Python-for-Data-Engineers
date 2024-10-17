785fdd4fef92e086a8a1b4a5d4466dc16885a774
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-10-17 23:20:19 +0300

    Update matlib.py

--- matlib.py
+++ matlib.py
@@ -76,3 +76,25 @@ plt.hist(life_exp1950, bins=15)
 # Show and clear plot again
 plt.show()
 plt.clf()
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+plt.title(title)
+
+plt.show()
+
+# Add title
+
+
+# After customizing, display the plot
